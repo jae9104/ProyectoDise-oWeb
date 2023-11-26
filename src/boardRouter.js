90d52a2fd737aeb09fc71bdc0f2aceb60f2a2323
IvanGutierrrez <146970754+IvanGutierrrez@users.noreply.github.com>
2023-11-26 18:45:27 +0100

    Update boardRouter.js

--- src/boardRouter.js
+++ src/boardRouter.js
@@ -1,1 +1,43 @@
+import express from 'express';
+import * as boardService from './boardService.js';
 
+const router = express.Router();
+/*Muestra todas las publicaciones */
+router.get('/', (req, res) => {
+    res.render('index', { 
+        posts: boardService.getPostsSummary() 
+    });
+});
+
+/*Modifica los valores que tiene dentro de cada post */
+router.post('/post/new', upload.single('imagen'), (req, res) => {
+    let { nombre, precio, mano, kilometros, combustible, transmision, caballos, descripcion } = req.body;
+    let imagen = req.file;  // Esto depende de cómo multer almacena la imagen
+
+    // Luego, puedes pasar las nuevas propiedades al objeto post
+    boardService.addPost({ nombre, precio, mano, kilometros, combustible, transmision, caballos, descripcion, imagen });
+
+    res.render('saved_post');
+});
+
+/*Muestra los detalles de una publicación específica en función de su identificador */
+router.get('/post/:id', (req, res) => {
+
+    let post = boardService.getPost(req.params.id);
+
+    res.render('show_post', { post });
+});
+
+/* Elimina una publicación específica en función de su identificador */
+router.get('/post/:id', (req, res) => {
+    let post = boardService.getPostDetails(req.params.id);
+
+    if (post) {
+        res.render('show_post', { post });
+    } else {
+        // Manejar el caso en el que no se encuentra el post con el ID proporcionado
+        res.render('post_not_found');
+    }
+});
+
+export default router;
