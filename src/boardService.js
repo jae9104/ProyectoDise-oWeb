e5bff9591a5381aa68a72412e2f5d6e3dd2a122c
IvanGutierrrez <146970754+IvanGutierrrez@users.noreply.github.com>
2023-11-28 10:19:03 +0100

    Update boardService.js

--- src/boardService.js
+++ src/boardService.js
@@ -1,10 +1,9 @@
 const posts = new Map();
 let nextId = 0;
 
-addPost({ nombre: "Audi R8", precio: "100000", mano: "1º mano", kilometros: "0-10k", combustible: "Gasolina", transmision: "Manual", caballos: "500", descripcion: "Mejor coche", imagen: "https://cdn.autobild.es/sites/navi.axelspringer.es/public/media/image/2022/10/audi-r8-v10-gt-2023-2831875.jpg"  });
-addPost({ nombre: "BMW M3", precio: "25000", mano: "2º mano", kilometros: "0-10k", combustible: "Gasolina", transmision: "Manual", caballos: "750", descripcion: "Rapidisimo", imagen: "https://hips.hearstapps.com/hmg-prod/images/bmw-m3-cs-2024-1674639702.jpg"  });
-addPost({ nombre: "Audi R8", precio: "100000", mano: "1º mano", kilometros: "0-10k", combustible: "Gasolina", transmision: "Manual", caballos: "500", descripcion: "Mejor coche", imagen: "https://cdn.autobild.es/sites/navi.axelspringer.es/public/media/image/2022/10/audi-r8-v10-gt-2023-2831875.jpg"  });
-addPost({ nombre: "BMW M3", precio: "25000", mano: "2º mano", kilometros: "0-10k", combustible: "Gasolina", transmision: "Manual", caballos: "750", descripcion: "Rapidisimo", imagen: "https://hips.hearstapps.com/hmg-prod/images/bmw-m3-cs-2024-1674639702.jpg"  });
+addPost({ nombre: "Audi R8", precio: "250000", mano: "1º mano", kilometros: "0-10k", combustible: "Gasolina", transmision: "Manual", caballos: "400", descripcion: "Mejor coche para curvasa", imagen: "https://cdn.autobild.es/sites/navi.axelspringer.es/public/media/image/2022/10/audi-r8-v10-gt-2023-2831875.jpg"  });
+addPost({ nombre: "BMW M3", precio: "180000", mano: "2º mano", kilometros: "50k-100k", combustible: "Diesel", transmision: "Manual", caballos: "2500", descripcion: "Rapidisimo", imagen: "https://hips.hearstapps.com/hmg-prod/images/bmw-m3-cs-2024-1674639702.jpg"  });
+addPost({ nombre: "Nissan GTR", precio: "100000", mano: "3º mano", kilometros: "100k-200k", combustible: "Gasolina", transmision: "Automatico", caballos: "550", descripcion: "Mejor coche en las rectas", imagen: "https://hips.hearstapps.com/hmg-prod/images/nissan-gtr-2022-japon-portada-1631625941.jpg"  });
 
 export function addPost(post) {
     let id = nextId++;
