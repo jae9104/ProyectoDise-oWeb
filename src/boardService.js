e1cb426552ac6e33f4b33ea6bd9b142287dd404e
jae9104 <146970727+jae9104@users.noreply.github.com>
2023-11-30 14:13:56 +0100

    Update boardService.js

--- src/boardService.js
+++ src/boardService.js
@@ -1,10 +1,13 @@
 const posts = new Map();
-const opiniones = new Map();
+const opiniones = {};
 let nextId = 0;
 
 addPost({ nombre: "Audi R8", precio: "250000", mano: "1º mano", kilometros: "0-10k", combustible: "Gasolina", transmision: "Manual", caballos: "400", descripcion: "Mejor coche para curvasa", imagen: "https://cdn.autobild.es/sites/navi.axelspringer.es/public/media/image/2022/10/audi-r8-v10-gt-2023-2831875.jpg"  });
 addPost({ nombre: "BMW M3", precio: "180000", mano: "2º mano", kilometros: "50k-100k", combustible: "Diesel", transmision: "Manual", caballos: "2500", descripcion: "Rapidisimo", imagen: "https://hips.hearstapps.com/hmg-prod/images/bmw-m3-cs-2024-1674639702.jpg"  });
 addPost({ nombre: "Nissan GTR", precio: "100000", mano: "3º mano", kilometros: "100k-200k", combustible: "Gasolina", transmision: "Automatico", caballos: "550", descripcion: "Mejor coche en las rectas", imagen: "https://hips.hearstapps.com/hmg-prod/images/nissan-gtr-2022-japon-portada-1631625941.jpg"  });
+
+agregarOpinion(1,{nombre: "Jorge", email: "jorge@gmail.com", valoracion: "8", comentario: "Brutal coche, con estética de locos"});
+agregarOpinion(1,{nombre: "Ivan", email: "ivan@gmail.com", valoracion: "3", comentario: "Un poco flojo el acabado interior"})
 
 export function addPost(post) {
     let id = nextId++;
@@ -54,17 +57,18 @@ export function getPostDetails(id) {
 ///////////////////sección opiniones///////////////////////
 
 
-export function agregarOpinion(opinion){
-        
-        opiniones.set(opinion.nombre,opinion);
-    }
+export function agregarOpinion(id,opinion){       
+        if (!opiniones[id]) {
+            opiniones[id] = [];
+        }
 
-export function obtenerOpiniones(){
-    return Array.from(opiniones.values()).map(opinion => ({
-        nombre: opinion.nombre,
-        email: opinion.email,
-        valoracion: opinion.email,
-        comentario: opinion.comentario
-    }));
-    }
+        opinion.id = id.toString();
+        opiniones[id].push(opinion);
+        
+}
+
+export function obtenerOpiniones(idpag){
+    return opiniones[idpag] || [];
+}
+
 
