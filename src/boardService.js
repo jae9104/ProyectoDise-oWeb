ded617cee6f2bbafe4d59ed36dd6f5ee638a7866
IvanGutierrrez <146970754+IvanGutierrrez@users.noreply.github.com>
2023-11-30 16:38:57 +0100

    Update boardService.js

--- src/boardService.js
+++ src/boardService.js
@@ -9,10 +9,71 @@ addPost({ nombre: "Nissan GTR", precio: "100000", mano: "3º mano", kilometros: 
 agregarOpinion(1,{nombre: "Jorge", email: "jorge@gmail.com", valoracion: "8", comentario: "Brutal coche, con estética de locos"});
 agregarOpinion(1,{nombre: "Ivan", email: "ivan@gmail.com", valoracion: "3", comentario: "Un poco flojo el acabado interior"})
 
-export function addPost(post) {
-    let id = nextId++;
-    post.id = id.toString();
-    posts.set(post.id, post);
+function validarFormulario(post,fallo = {}) {
+    // Validación básica
+    if (post.nombre === "") {
+        fallo.message ="Complete el campo obligatorio de Marca y modelo del coche";
+        return false;
+    }
+    if (post.imagen === "") {
+        fallo.message = "Complete el campo obligatorio de Dirección de la imagen";
+        return false;
+    }
+    if (post.precio === "") {
+        fallo.message = "Complete el campo obligatorio de Precio estimado";
+        return false;
+    }
+    if (post.mano === "" || post.mano === "Selecciona una opción") {
+        fallo.message = "Complete el campo obligatorio de Mano";
+        return false;
+    }
+    if (post.kilometros === "" || post.kilometros === "Selecciona una opción") {
+        fallo.message = "Complete el campo obligatorio de Kilometros";
+        return false;
+    }
+    if (post.combustible === "" || post.combustible === "Selecciona una opción") {
+        fallo.message = "Complete el campo obligatorio de Combustible";
+        return false;
+    }
+    if (post.transmision === "" || post.transmision === "Selecciona una opción") {
+        fallo.message = "Complete el campo obligatorio de Transmision";
+        return false;
+    }
+    if (post.caballos === "") {
+        fallo.message = "Complete el campo obligatorio de Caballos de potencia";
+        return false;
+    }
+    if (post.descripcion === "") {
+        fallo.message = "Complete el campo obligatorio de Descripcion y/o defectos";
+        return false;
+    }
+  
+    // Validacion para valores dentro de los rangos
+    if (parseInt(post.precio) < 0) {
+        fallo.message = "El precio no puede ser negativo";
+        return false;
+    }
+    if (parseInt(post.caballos) < 0 || parseInt(post.caballos) > 1200) {
+        fallo.message = "Los caballos deben estar entre 0 y 1200";
+        return false;
+      }
+  
+  
+    // Si todas las validaciones pasan, el formulario se envía
+    return true;
+}
+
+export function addPost(post,ok,fallo) {
+    if (validarFormulario(post,fallo)){
+        let id = nextId++;
+        post.id = id.toString();
+        posts.set(post.id, post);
+        ok = true;
+    }
+    else{ 
+        ok = false;
+    }
+    return ok, fallo;
 }
 
 export function deletePost(id){
@@ -70,5 +131,3 @@ export function agregarOpinion(id,opinion){
 export function obtenerOpiniones(idpag){
     return opiniones[idpag] || [];
 }
-
-
