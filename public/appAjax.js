0d86badda19d257f154fd18ce0e6538966b1d8ae
IvanGutierrrez <146970754+IvanGutierrrez@users.noreply.github.com>
2023-12-15 21:02:21 +0100

    Update appAjax.js

--- public/appAjax.js
+++ public/appAjax.js
@@ -1,18 +1,25 @@
 const NUM_RESULTS = 4;
 let loadMoreRequests = 0;
+let faltanAun = true;
 
-async function loadMore(){
+async function loadMore() {
+    if (faltanAun) {
+        const from = (loadMoreRequests + 1) * NUM_RESULTS;
+        const to = from + NUM_RESULTS;
 
-    const from = (loadMoreRequests+1) * NUM_RESULTS;
-    const to = from + NUM_RESULTS;
+        const response = await fetch(`/coches?from=${from}&to=${to}`);
+        const newCoches = await response.text();
 
-    const response = await fetch(`/coches?from=${from}&to=${to}`);
-    const newCoches = await response.text();
-  
-    const cochesDiv = document.getElementById("coches");
+        const cochesDiv = document.getElementById("coches");
 
-    cochesDiv.innerHTML += newCoches;
+        cochesDiv.innerHTML += newCoches;
 
-    loadMoreRequests++;
+        if (newCoches.trim() === '') {
+            faltanAun = false;
+            document.getElementById("CargarMas").style.display = "none";
+        }
+
+        loadMoreRequests++;
+
+    }
 }
-    
