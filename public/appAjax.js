0d1a0a8671a3a5c5df26dbdd6da0048f85bcf57d
IvanGutierrrez <146970754+IvanGutierrrez@users.noreply.github.com>
2023-12-15 22:12:15 +0100

    Update appAjax.js

--- public/appAjax.js
+++ public/appAjax.js
@@ -1,31 +1,28 @@
 const NUM_RESULTS = 4;
 let loadMoreRequests = 0;
-let faltanAun = true;
 
 async function loadMore() {
-    if (faltanAun) {
-        const from = (loadMoreRequests + 1) * NUM_RESULTS;
-        const to = from + NUM_RESULTS;
+    const from = (loadMoreRequests + 1) * NUM_RESULTS;
+    const to = from + NUM_RESULTS;
 
-        const response = await fetch(`/coches?from=${from}&to=${to}`);
-        const newCoches = await response.text();
+    const response = await fetch(`/coches?from=${from}&to=${to}`);
+    const newCoches = await response.text();
 
-        const cochesDiv = document.getElementById("coches");
+    const cochesDiv = document.getElementById("coches");
 
-        cochesDiv.innerHTML += newCoches;
-        
-        loadMoreRequests++;
+    cochesDiv.innerHTML += newCoches;
 
-        // Verificar si los siguientes 4 coches están vacíos
-        const nextFrom = (loadMoreRequests + 1) * NUM_RESULTS;
-        const nextTo = nextFrom + NUM_RESULTS;
+    loadMoreRequests++;
 
-        const nextResponse = await fetch(`/coches?from=${nextFrom}&to=${nextTo}`);
-        const nextCochesHTML = await nextResponse.text();
+    // Verificar si los siguientes 4 coches están vacíos
+    const nextFrom = (loadMoreRequests + 1) * NUM_RESULTS;
+    const nextTo = nextFrom + NUM_RESULTS;
 
-        if (nextCochesHTML.trim() === '') {
-            faltanAun = false;
-            document.getElementById("CargarMas").style.display = "none";
-        }
+    const nextResponse = await fetch(`/coches?from=${nextFrom}&to=${nextTo}`);
+    const nextCochesHTML = await nextResponse.text();
+
+    if (nextCochesHTML.trim() === '') {
+        document.getElementById("CargarMas").style.display = "none";
     }
+
 }
