bd3cd1ad448b0c39fa6cf8d185d0f26764a462ec
IvanGutierrrez <146970754+IvanGutierrrez@users.noreply.github.com>
2023-12-19 19:15:35 +0100

    Update addAjax2.js

--- public/addAjax2.js
+++ public/addAjax2.js
@@ -1,14 +1,17 @@
 document.addEventListener("DOMContentLoaded", function() {
     const AñadidoOK = document.getElementById("Añadido");//Ya esta añadido al carrito
     const carritoButton = document.getElementById("carrito");//Añadir al carrito
+    const QuitarButton = document.getElementById("Quitarcarrito");//Quitar del carrito
 
     const compra = carritoButton.getAttribute("data-compra") === "true";
 
     if (compra) {
         AñadidoOK.style.display = "block";
+        QuitarButton.style.display = "block";
         carritoButton.style.display = "none";
     } else {
         AñadidoOK.style.display = "none";
+        QuitarButton.style.display = "none";
         carritoButton.style.display = "block";
     }
 });
@@ -18,5 +21,16 @@ async function addToCarrito(postId) {
     if (response.ok) {
         document.getElementById("carrito").style.display = "none";
         document.getElementById("Añadido").style.display = "block";
+        document.getElementById("Quitarcarrito").style.display = "block";
+    }
+}
+
+async function deleteToCarrito(postId) {
+    const response = await fetch(`/deleteToCarrito?postId=${postId}`);
+
+    if (response.ok) {
+        document.getElementById("carrito").style.display = "block";
+        document.getElementById("Añadido").style.display = "none";
+        document.getElementById("Quitarcarrito").style.display = "none";
     }
 }
